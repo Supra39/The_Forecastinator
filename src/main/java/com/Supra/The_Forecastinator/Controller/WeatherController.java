7df5f380acb769623323542ca16a5567b1f07ae1
Supra39 <erik.7337@gmail.com>
2024-11-22 17:57:38 +0100

    Update to WeatherController to use @pathvariable
    changed the class to use @PathVariable istead of @RequestParam. Currently none of the variables are optional so it makes more sense to only @PathVariable, to make the URL easier to type. If the code gets updated to have optional variables those will probably be @requestparam

--- src/main/java/com/Supra/The_Forecastinator/Controller/WeatherController.java
+++ src/main/java/com/Supra/The_Forecastinator/Controller/WeatherController.java
@@ -12,27 +12,27 @@ public class WeatherController {
 
     @Autowired
     private WeatherService weatherService;
-
-    @GetMapping("/current") // get current weather
+    //changed the class to use @PathVariable istead of @RequestParam, in my case none of the variables are optional so it makes more sense to only use @PathVariable, to make the URL easier to type.
+    @GetMapping("/current/{latitude}/{longitude}") // get current weather, updated to use PathVariable to make the URL more intuitive to write
     public ResponseEntity<WeatherResponse> getCurrentWeather(
-            @RequestParam double latitude,
-            @RequestParam double longitude) {
+            @PathVariable double latitude,
+            @PathVariable double longitude) {
         return ResponseEntity.ok(weatherService.getCurrentWeather(latitude, longitude));
     }
 
-    @GetMapping("/forecast") //get upcoming weather
+    @GetMapping("/forecast/{latitude}/{longitude}") //get upcoming weather, updated to use PathVariable to make the URL more intuitive to write
     public ResponseEntity<WeatherResponse> getForecast(
-            @RequestParam double latitude,
-            @RequestParam double longitude) {
+            @PathVariable double latitude,
+            @PathVariable double longitude) {
         return ResponseEntity.ok(weatherService.getForecast(latitude, longitude));
     }
 
-    @GetMapping("/historical") //get historial weather
+    @GetMapping("/historical/{latitude}/{longitude}/{startDate}/{endDate}") //get historial weather, updated to use PathVariable to make the URL more intuitive to write
     public ResponseEntity<WeatherResponse> getHistoricalWeather(
-            @RequestParam double latitude,
-            @RequestParam double longitude,
-            @RequestParam String startDate,
-            @RequestParam String endDate) {
+            @PathVariable double latitude,
+            @PathVariable double longitude,
+            @PathVariable String startDate,
+            @PathVariable String endDate) {
         return ResponseEntity.ok(weatherService.getHistoricalWeather(latitude, longitude, startDate, endDate));
     }
 
